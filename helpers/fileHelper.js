3812d802748c1eec67c43809ce30a027f48d75bf
Nathaniel Carter <nhcarer@wpi.edu>
2021-09-24 20:57:47 -0500

    change error handling

--- helpers/fileHelper.js
+++ helpers/fileHelper.js
@@ -7,14 +7,15 @@ class FileHelper {
         const scoreString = line.substring(0, centerIndex);
         const jsonString = line.substring(centerIndex + 1);
 
-        const score = parseInt(scoreString)
+        const score = parseInt(scoreString);
 
         try {
             const data = JSON.parse(jsonString);
 
             return {score, id: data.id, index};
         } catch (err) {
-            throw new Error(`invalid json format No JSON object could be decoded: ${err}`);
+            console.log(`invalid json format No JSON object could be decoded: ${err}`);
+            process.exit(1);
         }
     }
 
@@ -28,20 +29,18 @@ class FileHelper {
             });
 
             readInterface.on("line", (line) => {
-                const data = FileHelper.parseLine(line, index)
+                const data = FileHelper.parseLine(line, index);
 
                 lines.push(data);
                 index++;
 
                 if (index >= limit) {
                     readInterface.close();
-                    readInterface.removeAllListeners()
+                    readInterface.removeAllListeners();
                 }
             });
 
-            readInterface.on("close", () => {
-                resolve(lines)
-            });
+            readInterface.on("close", () => resolve(lines));
         });
     }
 }
--- highest.js
+++ highest.js
@@ -1,12 +1,13 @@
 const FileHelper = require('./helpers/fileHelper');
 
 const main = async () => {
-    const args = process.argv.slice(2)
+    const args = process.argv.slice(2);
 
     try {
         await highest(args[0], args[1]);
     } catch (err) {
-        throw new Error(`Runtime error: ${err}`);
+        console.log(`Runtime error: ${err}`);
+        process.exit(1);
     }
 }
 
@@ -15,16 +16,16 @@ const highest = async (filepath, limit) => {
     const lines = await FileHelper.readFile(filepath, limit);
 
     const sortedLines = lines.sort((a, b) => {
-        const diff = b.score - a.score
+        const diff = b.score - a.score;
 
         if (diff === 0) {
             return b.index - a.index;
         }
 
-        return diff
+        return diff;
     });
 
-    console.log(sortedLines)
+    console.log(sortedLines);
 }
 
 main()
