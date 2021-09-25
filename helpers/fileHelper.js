21dd08927977e6bebe716ccbce5544e3f239e406
Nathaniel Carter <nhcarer@wpi.edu>
2021-09-24 20:53:03 -0500

    implement highest.js

---
+++ helpers/fileHelper.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const readline = require('readline');
+
+class FileHelper {
+    static parseLine(line, index) {
+        const centerIndex = line.indexOf(': ');
+        const scoreString = line.substring(0, centerIndex);
+        const jsonString = line.substring(centerIndex + 1);
+
+        const score = parseInt(scoreString)
+
+        try {
+            const data = JSON.parse(jsonString);
+
+            return {score, id: data.id, index};
+        } catch (err) {
+            throw new Error(`invalid json format No JSON object could be decoded: ${err}`);
+        }
+    }
+
+    static async readFile(filepath, limit) {
+        return new Promise((resolve) => {
+            let index = 0;
+            const lines = [];
+
+            const readInterface = readline.createInterface({
+                input: fs.createReadStream(filepath),
+            });
+
+            readInterface.on("line", (line) => {
+                const data = FileHelper.parseLine(line, index)
+
+                lines.push(data);
+                index++;
+
+                if (index >= limit) {
+                    readInterface.close();
+                    readInterface.removeAllListeners()
+                }
+            });
+
+            readInterface.on("close", () => {
+                resolve(lines)
+            });
+        });
+    }
+}
+
+module.exports = FileHelper;
---
+++ highest.js
@@ -0,0 +1,30 @@
+const FileHelper = require('./helpers/fileHelper');
+
+const main = async () => {
+    const args = process.argv.slice(2)
+
+    try {
+        await highest(args[0], args[1]);
+    } catch (err) {
+        throw new Error(`Runtime error: ${err}`);
+    }
+}
+
+
+const highest = async (filepath, limit) => {
+    const lines = await FileHelper.readFile(filepath, limit);
+
+    const sortedLines = lines.sort((a, b) => {
+        const diff = b.score - a.score
+
+        if (diff === 0) {
+            return b.index - a.index;
+        }
+
+        return diff
+    });
+
+    console.log(sortedLines)
+}
+
+main()
